a9e9c6876ebb6029420faac0fed9a29240c8a6d3
Ratchaphoon <flukefluke4069@gmail.com>
2018-09-12 16:40:10 +0700

    Comsead App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter} from 'react-router-dom'
+import * as firebase from 'firebase'
+import {Router} from 'react-router-redux'
+import {createStore, combineReducers, applyMiddleware,} from 'redux'
+import logger from 'redux-logger'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const mylongger = (store) => (next) => (action) =>{
+    console.log("Logged Action",action);
+    next(action);
+}
+
+//Firebase Install
+var config = {
+    apiKey: "AIzaSyAVeePOF1l-y6an6_Fcr6YoVK3-QleBqjk",
+    authDomain: "comseadreact.firebaseapp.com",
+    databaseURL: "https://comseadreact.firebaseio.com",
+    projectId: "comseadreact",
+    storageBucket: "comseadreact.appspot.com",
+    messagingSenderId: "843346359196"
+  };
+  firebase.initializeApp(config);
+//Browerrouter & Reduxrouter
+  ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+        <App/>
+        </Provider> 
+    </BrowserRouter> ,
+    document.getElementById('root'));
 registerServiceWorker();
