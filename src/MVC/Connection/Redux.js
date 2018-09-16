a9806eb9a005cda3ba50df256541eb9eb19ecd5e
Ratchaphoon <flukefluke4069@gmail.com>
2018-09-16 20:16:27 +0700

    Add files

--- src/MVC/Connection/Redux.js
+++ src/MVC/Connection/Redux.js
@@ -3,10 +3,14 @@ import * as redux from 'redux'
 import logger from 'redux-logger'
 
 
-
-
 // Redux logger//
 const mylongger = (store) => (next) => (action) =>{
     console.log("Logged Action",action);
     next(action);
 }
+ // Store 
+const store = createStore(combineReducers({user : Userreducer,status: StatusLogger,statususer : StatusUser,reservationroom : ReservationRoom,login : Login})
+,{}
+,applyMiddleware(mylongger,logger)
+
+);
