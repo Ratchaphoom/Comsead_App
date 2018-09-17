e9fce31de0741fd89eeb87052f249eff5f5e1f3e
Ratchaphoon <flukefluke4069@gmail.com>
2018-09-18 00:07:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch,NavLink } from 'react-router-dom'
+import 'mdbreact/dist/css/mdb.css';
+import WOW from 'wowjs'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 class App extends Component {
   render() {
+    new WOW.WOW().init();    
+      AOS.init();
+  // You can also pass an optional settings object
+  // below listed default settings
+    AOS.init({
+      // Global settings
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+
+      
+      // Settings that can be overriden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
     return (
       <div className="App">
         <header className="App-header">
